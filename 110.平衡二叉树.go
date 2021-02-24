7990e52814f7533b7957d388878838c53cd35437
fozhyn <fozhyn@gmail.com>
2021-02-24 11:09:45 +0800

    chore: sync

---
+++ 110.平衡二叉树.go
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=golang
+ *
+ * [110] 平衡二叉树
+ *
+ * https://leetcode-cn.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (54.71%)
+ * Likes:    558
+ * Dislikes: 0
+ * Total Accepted:    163.8K
+ * Total Submissions: 297.3K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ *
+ * 本题中，一棵高度平衡二叉树定义为：
+ *
+ *
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：true
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,2,2,3,3,null,null,4,4]
+ * 输出：false
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = []
+ * 输出：true
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中的节点数在范围 [0, 5000] 内
+ * -10^4
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isBalanced(root *TreeNode) bool {
+	return height(root) >= 0
+}
+
+func height(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	leftHeight := height(root.Left)
+	rightHeight := height(root.Right)
+
+	if leftHeight == -1 || rightHeight == -1 || leftHeight-rightHeight < -1 || leftHeight-rightHeight > 1 {
+		return -1
+	}
+
+	if leftHeight > rightHeight {
+		return leftHeight + 1
+	}
+
+	return rightHeight + 1
+}
+
+// @lc code=end
+
